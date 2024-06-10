417857431202d890296060422dc03ac254bbb01d
Malav Shah <malav@mobiuso.com>
2024-06-10 13:48:59 +0530

    change workflow

--- GithubActionCheck.py
+++ GithubActionCheck.py
@@ -1,4 +1,5 @@
 import yaml
+import os
 
 # Function to write data to YAML file
 def write_yaml(filename, data):
@@ -12,24 +13,12 @@ def read_yaml(filename):
     return data
 
 # Filename
-filename = 'data.yaml'
+filename = ""
 
-# Data to write to YAML
-data = {
-    'name': 'John Doe',
-    'age': 30,
-    'occupation': 'Software Engineer',
-    'languages': ['Python', 'Java', 'JavaScript'],
-    'contact': {
-        'email': 'john.doe@example.com',
-        'phone': '123-456-7890'
-    }
-}
 
 # Write data to YAML file
-write_yaml(filename, data)
 data2 = read_yaml(filename)
-print(data2['contact'])
+print(data2['name'])
 print(f'Wrote initial data to {filename}')
 
 
--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,6 @@
 print("hello world")
+print("how are you!")
+
+# To run the command and change the yaml file 
+#or
+# To prepare the yaml file before publish to github action
